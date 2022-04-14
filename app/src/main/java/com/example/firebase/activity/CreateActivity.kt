fb4b884a64b5868dabf44becee02fdaf9967895f
HP <rodilbek@gmail.com>
2022-04-14 13:02:18 +0300

    Storage

--- app/src/main/java/com/example/firebase/activity/CreateActivity.kt
+++ app/src/main/java/com/example/firebase/activity/CreateActivity.kt
@@ -1,20 +1,37 @@
 package com.example.firebase.activity
 
+import android.app.Activity
+import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.net.toUri
+import com.bumptech.glide.Glide
 import com.example.firebase.R
 import com.example.firebase.manager.DatabaseHandler
 import com.example.firebase.manager.DatabaseManager
+import com.example.firebase.manager.StorageHandler
+import com.example.firebase.manager.StorageManager
 import com.example.firebase.model.Post
 import com.sangcomz.fishbun.FishBun
+import com.sangcomz.fishbun.adapter.image.impl.GlideAdapter
+import com.yanzhenjie.album.Action
+import com.yanzhenjie.album.Album
+import com.yanzhenjie.album.AlbumFile
+import com.yanzhenjie.album.AlbumLoader
+import java.io.File
+import android.graphics.Bitmap
+import com.sangcomz.fishbun.MimeType
+
 
 class CreateActivity : BaseActivity() {
     lateinit var iv_photo: ImageView
-    var pickedImage: Uri? = null
+    var pickedPhoto: Uri? = null
     var allPhotos = ArrayList<Uri>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,6 +45,13 @@ class CreateActivity : BaseActivity() {
         var et_title = findViewById<EditText>(R.id.et_title)
         var et_body = findViewById<EditText>(R.id.et_body)
         var b_create = findViewById<Button>(R.id.b_create)
+        iv_photo = findViewById(R.id.iv_photo)
+
+        val iv_camera = findViewById<ImageView>(R.id.iv_camera)
+        iv_camera.setOnClickListener {
+            pickUserPhoto()
+            //albumPhoto()
+        }
 
         iv_close.setOnClickListener {
             finish()
@@ -36,8 +60,30 @@ class CreateActivity : BaseActivity() {
             val title = et_title.text.toString().trim()
             val body = et_body.text.toString().trim()
             val post = Post(title, body)
+            storePost(post)
+        }
+    }
+
+    fun storePost(post: Post) {
+        if (pickedPhoto != null) {
+            storeStorage(post)
+        } else {
             storeDatabase(post)
         }
+    }
+
+    private fun storeStorage(post: Post) {
+        showLoading(this)
+        StorageManager.uploadPhoto(pickedPhoto!!, object : StorageHandler {
+            override fun onSuccess(imgUrl: String) {
+                post.img = imgUrl
+                storeDatabase(post)
+            }
+
+            override fun onError(exception: Exception) {
+                storeDatabase(post)
+            }
+        })
     }
 
 
@@ -62,8 +108,56 @@ class CreateActivity : BaseActivity() {
         finish()
     }
 
-    fun pickUserPhoto(){
-        FishBun.with(this)
-            .setImageAdapter()
+    fun pickUserPhoto() {
+            FishBun.with(this)
+                .setImageAdapter(GlideAdapter())
+                .setMaxCount(1)
+                .setMinCount(1)
+                .setCamera(true)
+                .exceptGif(true)
+                .setButtonInAlbumActivity(false)
+                .setCamera(true)
+                .exceptGif(true)
+                .setReachLimitAutomaticClose(true)
+                .setAllViewTitle("All")
+                .setMenuAllDoneText("All Done")
+                .exceptMimeType(listOf(MimeType.GIF))
+                .hasCameraInPickerPage(true)
+                .setButtonInAlbumActivity(true)
+                .setSelectedImages(allPhotos)
+                .startAlbumWithActivityResultCallback(photoLauncher)
+        
     }
+
+    fun albumPhoto() {
+//        Album.camera(this)
+//            .image()
+//            .filePath("")
+//            .onResult(object : Action<String> {
+//                override fun onAction(result: String) {
+//                    Log.d("#####", result.toString())
+//                    iv_photo.setImageURI(result.toUri())
+//
+//                    val finalBitmap =
+//                        BitmapFactory.decodeFile(result)
+//                    iv_photo.setImageBitmap(finalBitmap)
+//
+//                }
+//
+//            }).onCancel(object : Action<String> {
+//                override fun onAction(result: String) {
+//                }
+//
+//            }).start()
+    }
+
+    val photoLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == Activity.RESULT_OK) {
+                allPhotos =
+                    it.data?.getParcelableArrayListExtra(FishBun.INTENT_PATH) ?: arrayListOf()
+                pickedPhoto = allPhotos.get(0)
+                iv_photo.setImageURI(pickedPhoto)
+            }
+        }
 }
--- app/src/main/java/com/example/firebase/adapter/PostAdapter.kt
+++ app/src/main/java/com/example/firebase/adapter/PostAdapter.kt
@@ -1,5 +1,7 @@
 package com.example.firebase.adapter
 
+import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -7,12 +9,14 @@ import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.firebase.MainActivity
 import com.example.firebase.R
 import com.example.firebase.model.Post
 
 class PostAdapter(var activity: MainActivity, var items: ArrayList<Post>) :
     RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_post_list, parent, false)
         return PostViewHolder(view)
@@ -26,13 +30,19 @@ class PostAdapter(var activity: MainActivity, var items: ArrayList<Post>) :
             val ll_post = holder.ll_post
             val iv_update = holder.iv_update
             val iv_delete = holder.iv_delete
+            val iv_photo = holder.iv_photo
 
             iv_delete.setOnClickListener {
                 activity.apiDeletePost(item)
             }
 
+
             tv_title.text = item.title!!.toUpperCase()
             tv_body.text = item.body
+            if (item.img.length > 0) {
+                Glide.with(iv_photo.context).load(item.img).into(iv_photo)
+                Log.d("@@@", item.img)
+            }
         }
     }
 
@@ -46,6 +56,7 @@ class PostAdapter(var activity: MainActivity, var items: ArrayList<Post>) :
         val ll_post: LinearLayout
         val iv_update: ImageView
         val iv_delete: ImageView
+        val iv_photo: ImageView
 
         init {
             tv_title = view.findViewById(R.id.tv_title)
@@ -53,6 +64,7 @@ class PostAdapter(var activity: MainActivity, var items: ArrayList<Post>) :
             ll_post = view.findViewById(R.id.ll_post)
             iv_update = view.findViewById(R.id.iv_update)
             iv_delete = view.findViewById(R.id.iv_delete)
+            iv_photo = view.findViewById(R.id.iv_photo)
         }
     }
     init {
